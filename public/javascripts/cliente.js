deb6e632b7db429c53d2e90bd31432b33564d2b6
Marta <margon@debian.debian.org>
2015-12-07 19:45:48 +0100

    cambio css

--- public/javascripts/cliente.js
+++ public/javascripts/cliente.js
@@ -2,9 +2,18 @@ $(document).ready(function(){
 
    var socket=io.connect('localhost:3000');//Establezco un socket con el servidor en este caso como es la misma máquina localhost
 
-		socket.on('connect',function(){
-			 var saludo="hola";
-			 socket.emit('saludo',saludo); // A modo de ejemplo emito un saludo
-			 
-			});
+	socket.on('connect',function(){
+		 var saludo="hola";
+		 socket.emit('saludo',saludo); // A modo de ejemplo emito un saludo
+		 
+		});
+
+	$("#numero").keyup(function(e) {
+		var code = e.which || e.keyCode;
+
+		if(code == 13) {
+			$("#respuestas").append("<li>"+$("#numero").val()+"</li>")
+		}
+	});
+
 });
